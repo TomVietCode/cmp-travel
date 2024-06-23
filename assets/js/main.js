a73eeb9d0ab97b0616d5ddf22165a21637b54d6f
TomVietCode <mcuong04.work@gmail.com>
2024-06-23 18:15:44 +0700

    Thêm button button go top cho các page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Footer
 const footer = document.querySelector("footer")
 if(footer){
   footer.innerHTML = `
@@ -45,5 +46,47 @@ if(footer){
       <p>Copyright @ CMP-2024. All Rights Reserved.</p>
     </div>
   `
-  
 }
+// End Footer
+
+// Scroll Event
+const scrollUpIcon = document.getElementById('scrollUpIcon');
+const scrollDownIcon = document.getElementById('scrollDownIcon');
+const scrollToPosition = 730; // Vị trí cuộn theo pixel (ví dụ: 500px từ đầu trang)
+
+let isAtScrollDownPosition = false; // Biến cờ để theo dõi vị trí cuộn
+if(scrollDownIcon && scrollUpIcon){
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+  
+    if (scrollTop >= scrollToPosition) {
+      isAtScrollDownPosition = true;
+      scrollUpIcon.style.display = 'block';
+      scrollDownIcon.style.display = 'none'; // Ẩn icon "mũi tên đi xuống" khi cuộn đến vị trí
+    } else {
+      isAtScrollDownPosition = false;
+      scrollUpIcon.style.display = 'none';
+      scrollDownIcon.style.display = 'block';
+    }
+  });
+  
+  scrollUpIcon.addEventListener('click', () => {
+    if (isAtScrollDownPosition) { // Kiểm tra trước khi cuộn lên đầu
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }
+  });
+  
+  scrollDownIcon.addEventListener('click', () => {
+    window.scrollTo({
+      top: scrollToPosition,
+      behavior: 'smooth'
+    });
+  });
+  
+  // Ẩn icon "mũi tên đi lên" ban đầu
+  scrollUpIcon.style.display = 'none';
+}
+// End Scroll Event
